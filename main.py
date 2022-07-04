9517843a0172120f9eb2c546bcf059e98c2ecb3b
Daniru2007 <daniruayuka@gmail.com>
2022-07-04 20:38:26 +0530

    searching and shutting down the computer

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
+import os
+import pywhatkit
 
 from commands import load_data, start_app
 
 load_data(json.load(open("data.json")))
+
 
 while True:
     try:
@@ -11,7 +14,20 @@ while True:
         exit()
     if command == "exit":
         exit()
+    if command == "cls":
+        os.system("cls")
+    if command == "whoami":
+        print(os.getlogin())
+    if command == "shutdown":
+        if input("are you sure?(y/n) ") == "y":
+            os.system("shutdown -s -t 0")
 
     if "open" in command:
-        if "chrome" in command:
-            start_app("chrome")
+        if "chrome" in command: start_app("chrome")
+        if "vscode" in command: start_app("vscode")
+        if "cmd" in command: start_app("cmd")
+        if "notion" in command: start_app("notion")
+
+    if "search" in command:
+        query = command.replace("search", "")
+        pywhatkit.search(query)
